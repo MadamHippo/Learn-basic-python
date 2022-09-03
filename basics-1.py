0213bfd8ace1d65896aacdbe351d54e07a84c60a
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-09-03 01:57:36 -0700

    Update basics-1.py

--- basics-1.py
+++ basics-1.py
@@ -101,3 +101,29 @@ Finally, make sure to add the third parameter of -1. This makes us increment by 
 ~ third -1 is increment, you start at the end and you go down 1 everytime.
 And you stop at -1 which stops you at 0.
 '''
+
+
+'''
+Larger Sum
+
+We are going to start our advanced challenge problems by calculating which list of two inputs has the larger sum. We will iterate through each of the list and calculate the sums, afterwards we will compare the two and return which one has a greater sum. Here are the steps we need:
+
+Define the function to accept two input parameters: lst1 and lst2
+Create two variables to record the two sums
+Loop through the first list and add up all of the numbers
+Loop through the second list and add up all of the numbers
+Compare the first and second sum and return the list with the greater sum
+'''
+
+def larger_sum(lst1, lst2):
+  sum1 = 0
+  sum2 = 0
+  for num1 in lst1:
+    sum1 += num1
+  for num2 in lst2:
+    sum2 += num2
+  if sum1 >= sum2:
+      return lst1
+  else:
+      return lst2
+  
