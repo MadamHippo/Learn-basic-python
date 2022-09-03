0c3626d5e19d86a963410c7b98e4f8ae22d3cc21
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-09-03 01:58:37 -0700

    Create basics-2.py

---
+++ basics-2.py
@@ -0,0 +1,29 @@
+'''
+CS 101 Path Codecademy...
+
+
+
+LIST INDICES
+
+Python list elements are ordered by index, a number referring to their placement in the list. List indices start at 0 and increment by one.
+
+~ remove() delete the matching element/object whereas del and pop removes the element at a specific index.
+~ del and pop deals with the index. The only difference between the two is that- pop return deleted the value from the list and del does not return anything.
+~ Pop is the only way that returns the object.
+~ Remove is the only one that searches objects (not index).
+
+~ If you want to delete a specific object in the list, use remove method.
+~ If you want to delete the object at a specific location (index) in the list, you can either use del or pop.
+~ Use the pop, if you want to delete and get the object at the specific location
+
+Which list would be created by running this code?
+
+list(range(2, 14, 4))
+2 = where you start.
+14 = where you should stop before.
+4 = how much you actually need to skip between.
+[2 , 6, 10 is the answer]
+
+'''
+
+
