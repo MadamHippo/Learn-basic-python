691c81be99d61771c78a17e9f0b1e92a3429050b
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-09-04 01:05:01 -0700

    Create basics-3-loops.py

---
+++ basics-3-loops.py
@@ -0,0 +1,7 @@
+'''
+For Loop
+
+In a for loop, we will know in advance how many times the loop will need to iterate because we will be working on a collection with a predefined length. With for loops, on each iteration, we will be able to perform an action on each element of the collection.
+
+
+'''
