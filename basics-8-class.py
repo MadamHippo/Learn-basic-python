74ba53cb6409556cd7b43ac736ce8726febf5e06
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-09-16 00:29:03 -0700

    Update basics-8-class.py

--- basics-8-class.py
+++ basics-8-class.py
@@ -90,5 +90,49 @@ print(y.class_variable) #I am a Class Variable!
  
 #basically the same exact example as above with grade()
 
+'''
+Methods
 
+Methods are functions that are defined as part of a class. 
+The first argument in a method is always the object that is calling the method. We name this first argument self
+We define methods similarly to functions, except that they are indented to be part of the class.
+'''
+      
+class Dog:
+  dog_time_dilation = 7
+ 
+  def time_explanation(self): # <- method
+    print("Dogs experience {} years for every 1 human year.".format(self.dog_time_dilation))
+ 
+pipi_pitbull = Dog()
+pipi_pitbull.time_explanation()
+# Prints "Dogs experience 7 years for every 1 human year."
+
+# Above we created a Dog class with a time_explanation method that takes one argument, self, which refers to the object calling the function. We created a Dog named pipi_pitbull and called the .time_explanation() method on our new object for Pipi.
+
+# Notice we didn’t pass any arguments when we called .time_explanation(), but were able to refer to self in the function body. When you call a method it automatically passes the object calling the method as the first argument.
+
+'''
+Methods in Classes with Arguments
+
+Methods always have at least this one argument.
+_Methods_ are _functions_ that can be called on instances of a class. They are a more specific name for a _function_ and are called using parentheses `()`. If we have a variable that contains an object instance, we can call methods on that instance as `instance_var.operation_name()`, where `instance_var` is the variable holding an instance, and `operation_name` is the name of the method we want to use. The `()` are required to tell Python to run the operation, and is used to provide any needed arguments.
+
+'''
+      
+# Dog class
+class Dog:
+  # Method of the class
+  def bark(self):
+    print("Ham-Ham")
+# Create a new instance
+charlie = Dog()
+ 
+# Call the method
+charlie.bark()
+# This will output "Ham-Ham"
+ 
+print(charlie.bark())
+      
+# We define methods similarly to functions, except that they are indented to be part of the class.
 
